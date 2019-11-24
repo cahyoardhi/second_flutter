a65742c325f1b7628adb766bda900db49c58d6eb
cahyoardhi <sebastiancahyoardhiiswara@gmail.com>
2019-11-24 11:07:31 +0700

    Create card for menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MovieApp());
+void main() => runApp(MoviesApp());
 
-class MovieApp extends StatelessWidget {
+class MoviesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Movie App',
+      title: 'NoMovie',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
@@ -20,11 +21,36 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie App'),
+        title: Text('NoMovie'),
       ),
-      body: Container(
-        child: Row(
-          children: <Widget>[],
+      body: Card(
+        margin: EdgeInsets.all(10),
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 100,
+                child: CachedNetworkImage(
+                  imageUrl:
+                      'https://image.tmdb.org/t/p/w600_and_h900_bestv2/hj8pyoNnynGeJTAbl7jcLZO8Uhx.jpg',
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                color: Colors.lightGreen,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text('Midway'),
+                    Text('12-12-2020'),
+                    Text('9.2'),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
