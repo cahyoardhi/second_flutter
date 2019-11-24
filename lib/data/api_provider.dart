b98076b0f1e160dfdbd3b12b405b4ab003a7483e
cahyoardhi <sebastiancahyoardhiiswara@gmail.com>
2019-11-24 11:35:39 +0700

    Create base Url

--- lib/data/api_provider.dart
+++ lib/data/api_provider.dart
@@ -1,5 +1,8 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' show Client;
 
 class ApiProvider {
   String apiKey = 'c5a4452c0e67d0999d0bc5c65d8ba011';
+  String baseUrl = 'https://api.themoviedb.org/3';
+
+  Client client = Client();
 }
